11ec391fe8f865001b9876079b9c56ca15f845e2
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-11-24 18:52:25 -0500

    Chapter 1 Exercise1

--- src/introtojava/Ex1.java
+++ src/introtojava/Ex1.java
@@ -1,9 +1,16 @@
-package chapter1;
+package introtojava;
 
 public class Ex1 {
 
+    /*
+    (Display three messages) Write a program that displays Welcome to Java,
+    Welcome to Computer Science, and Programming is fun.
+     */
+
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
+
+        System.out.println("Welcome To Java");
+        System.out.println("Welcome To Computer Science");
+        System.out.println("Programming is Fun");
     }
 }
