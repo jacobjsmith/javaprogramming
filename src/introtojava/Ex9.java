563e31525a239a557c21265ae4ab1b6245b5e250
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-11-25 12:56:11 -0500

    Chapter 1 - 9

---
+++ src/introtojava/Ex9.java
@@ -0,0 +1,18 @@
+package introtojava;
+
+public class Ex9 {
+
+    public static void main(String[] args) {
+        /*
+        (Area and perimeter of a rectangle) Write a program that displays the area and
+        perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following
+        formula:
+        area = width * height
+         */
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+        System.out.println("Area is " + area);
+    }
+}
