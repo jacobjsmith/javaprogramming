d6a95902eb768021851cb6bdf4813b1ee10875bb
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-11-24 19:10:41 -0500

    Chapter 1-2 Comment Addition

--- src/introtojava/Ex2.java
+++ src/introtojava/Ex2.java
@@ -4,6 +4,10 @@ public class Ex2 {
 
     public static void main(String[] args) {
 
+        /**
+         * (Display five messages) Write a program that displays Welcome to Java five times.
+         */
+
         for (int i = 0; i < 5; i++) {
             System.out.println("Welcome to Java");
         }
