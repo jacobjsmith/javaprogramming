c885c1abd7f345a4f21553fd1544c63ac343ed7f
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-11-30 20:40:46 -0500

    Chapter 2 - 13

---
+++ src/elementaryprogramming/Ex13.java
@@ -0,0 +1,24 @@
+package elementaryprogramming;
+
+import java.util.Scanner;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        /*
+        Write a program that prompts the user to enter a monthly saving amount and
+        displays the account value after the sixth month.
+         */
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the monthly saving amount: ");
+        double savingAmount = input.nextDouble();
+
+        double monthlyInterestRate = (1 + 0.00417);
+        double accountValue = 0;
+        for (int i = 0; i < 6; i++) {
+            accountValue = ((savingAmount + accountValue) * (monthlyInterestRate));
+        }
+
+        System.out.println("After the sixth month, the value is $" + accountValue);
+    }
+}
