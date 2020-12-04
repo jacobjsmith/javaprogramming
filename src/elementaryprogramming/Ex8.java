eb525dd52737888ef065880fc9ab3a06aa2714ca
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-12-03 23:55:08 -0500

    Chapter 2 - 8

---
+++ src/elementaryprogramming/Ex8.java
@@ -0,0 +1,12 @@
+package elementaryprogramming;
+
+public class Ex8 {
+
+    public static void main(String[] args) {
+        /*
+        (Current time) Listing 2.7, ShowCurrentTime.java, gives a program that displays
+        the current time in GMT. Revise the program so that it prompts the user to enter
+        the time zone offset to GMT and displays the time in the specified time zone.
+         */
+    }
+}
