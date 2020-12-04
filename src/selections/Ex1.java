16a2c69557a93c8c74444a85de1b4ee0e2b14e2e
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2020-12-04 17:36:47 -0500

    Chapter 3 - 1

---
+++ src/selections/Ex1.java
@@ -0,0 +1,32 @@
+package selections;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a, b, c: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        double discriminant = (b * b) * (4 * a * c);
+
+        double r1 = (-b + Math.sqrt(b * b) - (4 * a * c));
+        double r2 = (-b - Math.sqrt(b * b) - (4 * a * c));
+
+        if (discriminant > 0) {
+            System.out.println("The equation has two roots " + r1 + " and " + r2);
+        }
+        else if (discriminant == 0) {
+            System.out.println("The equation hs one root " + r1);
+        }
+        else {
+            System.out.println("The equation has no real roots");
+        }
+
+    }
+}
