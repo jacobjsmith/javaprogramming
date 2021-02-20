38366962221642cbe3b6544e4ece05c43e15f9ec
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2021-02-20 16:38:29 -0500

    Chapter 3 Ex5

---
+++ src/selections/Ex5.java
@@ -0,0 +1,44 @@
+package selections;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    /*
+    (Find future dates) Write a program that prompts the user to enter an integer for
+    today’s day of the week (Sunday is 0, Monday is 1, …, and Saturday is 6). Also
+    prompt the user to enter the number of days after today for a future day and display
+    the future day of the week.
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter today's day: ");
+        int today = input.nextInt();
+
+        switch (today) {
+            case 0: System.out.println("Sunday");break;
+            case 1: System.out.println("Monday");break;
+            case 2: System.out.println("Tuesday");break;
+            case 3: System.out.println("Wednesday");break;
+            case 4: System.out.println("Thursday");break;
+            case 5: System.out.println("Friday");break;
+            case 6: System.out.println("Saturday");break;
+        }
+
+        System.out.println("Enter the number of days elapsed since today: ");
+        int elapsedDays = input.nextInt();
+
+        int futureDay = (today + elapsedDays % 7);
+
+        switch (futureDay) {
+            case 0: System.out.println("Sunday");break;
+            case 1: System.out.println("Monday");break;
+            case 2: System.out.println("Tuesday");break;
+            case 3: System.out.println("Wednesday");break;
+            case 4: System.out.println("Thursday");break;
+            case 5: System.out.println("Friday");break;
+            case 6: System.out.println("Saturday");break;
+        }
+
+    }
+}
