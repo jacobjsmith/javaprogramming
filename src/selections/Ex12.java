02593d658920bef47313a94b7d5fc972ac3ba79b
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2021-02-24 19:55:49 -0500

    Chapter 3 Ex12

---
+++ src/selections/Ex12.java
@@ -0,0 +1,31 @@
+package selections;
+
+import java.util.Scanner;
+
+/*
+(Palindrome number) Write a program that prompts the user to enter a three-digit
+integer and determines whether it is a palindrome number. A number is palindrome
+if it reads the same from right to left and from left to right. Here is a sample
+run of this program:
+ */
+public class Ex12 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a three digit number: ");
+
+        String num = input.next();
+
+        char num1 = num.charAt(0);
+        char num2 = num.charAt(2);
+
+        if (num1 == num2) {
+            System.out.println("The number is a palindrome");
+        }
+        else {
+            System.out.println("The number is not a palindrome");
+        }
+    }
+}
